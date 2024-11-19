a00b574b6edd5f937a49f783b42349160f2a9683
Crabico <dernoncourshawna@gmail.com>
2024-11-19 15:29:55 +0100

    debut js

---
+++ jscript/test.js
@@ -0,0 +1,11 @@
+//console.log("hello wolrd")
+
+//let userName = "Crabico" //string
+//let age = 18; //int
+//let size = 1.80 //float
+//let isCool = true; //bool
+
+let a = 403
+let b = 404
+
+console.log(a+b, a/b, a-b, a*b, a%b)
